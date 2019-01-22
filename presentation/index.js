e7c0d119a618b77686da998d03daca0ad03fc58c
JLou <jlou.xd@gmail.com>
2019-01-22 21:49:18 +0100

    few notes

--- presentation/index.js
+++ presentation/index.js
@@ -1,5 +1,5 @@
 // Import React
-import React from 'react';
+import React from "react";
 // Import Spectacle Core tags
 import {
   Appear,
@@ -15,48 +15,48 @@ import {
   TableItem,
   TableRow,
   Notes
-} from 'spectacle';
+} from "spectacle";
 
-import Profile from './profiles';
-import Twitter from './twitter';
-import Note from './notes';
+import Profile from "./profiles";
+import Twitter from "./twitter";
+import Note from "./notes";
 // Import theme
-import createTheme from 'spectacle/lib/themes/default';
-import './style.css';
+import createTheme from "spectacle/lib/themes/default";
+import "./style.css";
 
 const images = {
-  netlifyFull: require('../assets/netlify-full.svg'),
-  domains: require('../assets/domains.svg'),
-  forms: require('../assets/forms.svg'),
-  identity: require('../assets/identity.svg'),
-  splitTesting: require('../assets/splitTesting.svg'),
-  pr: require('../assets/pr.svg'),
-  auth: require('../assets/auth.svg'),
-  notification: require('../assets/notification.svg'),
-  toml: require('../assets/netlifytoml.png'),
-  john: require('../assets/john.jpg'),
-  jl: require('../assets/jl.jpg'),
-  free: require('../assets/free.png'),
-  pro: require('../assets/pro.png'),
-  business: require('../assets/business.png'),
-  jam: require('../assets/jam.png'),
-  jamstackLogo: require('../assets/jamstackLogo.png'),
+  netlifyFull: require("../assets/netlify-full.svg"),
+  domains: require("../assets/domains.svg"),
+  forms: require("../assets/forms.svg"),
+  identity: require("../assets/identity.svg"),
+  splitTesting: require("../assets/splitTesting.svg"),
+  pr: require("../assets/pr.svg"),
+  auth: require("../assets/auth.svg"),
+  notification: require("../assets/notification.svg"),
+  toml: require("../assets/netlifytoml.png"),
+  john: require("../assets/john.jpg"),
+  jl: require("../assets/jl.jpg"),
+  free: require("../assets/free.png"),
+  pro: require("../assets/pro.png"),
+  business: require("../assets/business.png"),
+  jam: require("../assets/jam.png"),
+  jamstackLogo: require("../assets/jamstackLogo.png")
 };
 
 // Require CSS
-require('normalize.css');
+require("normalize.css");
 
 const theme = createTheme(
   {
-    primary: '#e74c3c',
-    secondary: '#ecf0f1',
-    tertiary: '#85144b',
-    quaternary: '#85144b',
-    dark: '#080F0F'
+    primary: "#e74c3c",
+    secondary: "#ecf0f1",
+    tertiary: "#85144b",
+    quaternary: "#85144b",
+    dark: "#080F0F"
   },
   {
-    primary: 'Montserrat',
-    secondary: 'Helvetica'
+    primary: "Montserrat",
+    secondary: "Helvetica"
   }
 );
 
@@ -65,9 +65,14 @@ const featureImageSize = 800;
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck transition={['slide']} transitionDuration={500} theme={theme} progress="bar">
+      <Deck
+        transition={["slide"]}
+        transitionDuration={500}
+        theme={theme}
+        progress="bar"
+      >
         <Slide bgColor="tertiary">
-          <Note presenter="john" notes={[]}/>
+          <Note presenter="john" notes={[]} />
           <Heading size={5} textColor="secondary">
             Simplify web deployments with
           </Heading>
@@ -92,14 +97,14 @@ export default class Presentation extends React.Component {
           github="jlou"
           pic={images.jl}
         />
-        <Slide transition={['zoom']} bgColor="primary">
-          <Note presenter="jl" notes={[]}/>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Note presenter="jl" notes={[]} />
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
             Existing Solutions
           </Heading>
         </Slide>
-        <Slide transition={['slide']} bgColor="primary">
-          <Note presenter="jl" notes={[]}/>
+        <Slide transition={["slide"]} bgColor="primary">
+          <Note presenter="jl" notes={[]} />
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
             Deploying by hand
           </Heading>
@@ -119,8 +124,8 @@ export default class Presentation extends React.Component {
           </List>
         </Slide>
 
-        <Slide transition={['slide']} bgColor="primary">
-          <Note presenter="jl" notes={[]}/>
+        <Slide transition={["slide"]} bgColor="primary">
+          <Note presenter="jl" notes={[]} />
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
             Github Pages
           </Heading>
@@ -141,8 +146,8 @@ export default class Presentation extends React.Component {
             </Appear>
           </List>
         </Slide>
-        <Slide transition={['slide']} bgColor="primary">
-          <Note presenter="jl" notes={[]}/>
+        <Slide transition={["slide"]} bgColor="primary">
+          <Note presenter="jl" notes={[]} />
           <Heading size={3} caps textColor="secondary">
             Continuous integration tools
           </Heading>
@@ -162,11 +167,14 @@ export default class Presentation extends React.Component {
           </List>
         </Slide>
         <Slide bgColor="secondary">
-          <Note presenter="john" notes={[]}/>
+          <Note presenter="john" notes={[]} />
           <Image src={images.netlifyFull} width={1000} />
         </Slide>
         <Slide bgColor="secondary">
-          <Note presenter="jl" notes={[]}/>
+          <Note
+            presenter="jl"
+            notes={["faire le site et push", "créer le site sur netlify"]}
+          />
           <Heading size={1} textColor="primary">
             Demo 1
           </Heading>
@@ -178,56 +186,61 @@ export default class Presentation extends React.Component {
           </List>
         </Slide>
         <Slide bgColor="secondary">
-          <Note presenter="john" notes={[]}/>
+          <Note presenter="john" notes={[]} />
           <Heading size={2} textColor="primary">
             Custom domains & DNS configuration
           </Heading>
           <Image src={images.domains} width={featureImageSize} />
         </Slide>
         <Slide bgColor="secondary">
-          <Note presenter="jl" notes={[]}/>
+          <Note
+            presenter="jl"
+            notes={[
+              "proposer aux participants d'aller sur chaptersplit.netlify.com"
+            ]}
+          />
           <Heading size={2} textColor="primary">
             Split testing
           </Heading>
           <Image src={images.splitTesting} width={featureImageSize} />
         </Slide>
         <Slide bgColor="secondary">
-          <Note presenter="jl" notes={[]}/>
+          <Note presenter="jl" notes={[]} />
           <Heading size={2} textColor="primary">
             Pull request deployment
           </Heading>
           <Image src={images.pr} width={featureImageSize} />
         </Slide>
         <Slide bgColor="secondary">
-          <Note presenter="john" notes={[]}/>
+          <Note presenter="john" notes={[]} />
           <Heading size={2} textColor="primary">
             Notification
           </Heading>
           <Image src={images.notification} width={featureImageSize} />
         </Slide>
         <Slide bgColor="secondary">
-          <Note presenter="john" notes={[]}/>
+          <Note presenter="john" notes={[]} />
           <Heading size={2} textColor="primary">
             Forms
           </Heading>
           <Image src={images.forms} width={featureImageSize} />
         </Slide>
         <Slide bgColor="secondary">
-          <Note presenter="john" notes={[]}/>
+          <Note presenter="john" notes={[]} />
           <Heading size={2} textColor="primary">
             Identity
           </Heading>
           <Image src={images.identity} width={featureImageSize} />
         </Slide>
         <Slide bgColor="secondary">
-          <Note presenter="jl" notes={[]}/>
+          <Note presenter="jl" notes={["jlou-test.netlify.com"]} />
           <Heading size={2} textColor="primary">
             Auth
           </Heading>
           <Image src={images.auth} width={featureImageSize} />
         </Slide>
         <Slide bgColor="secondary">
-          <Note presenter="john" notes={[]}/>
+          <Note presenter="john" notes={[]} />
           <Heading size={1} textColor="primary" caps>
             Demo 2
           </Heading>
@@ -240,25 +253,33 @@ export default class Presentation extends React.Component {
           </List>
         </Slide>
         <Slide bgColor="primary">
-          <Note presenter="jl" notes={[]}/>
+          <Note presenter="jl" notes={[]} />
           <Heading size={1} fit textColor="secondary" caps>
             But also...
           </Heading>
         </Slide>
         <Slide bgColor="secondary">
-          <Note presenter="jl" notes={[]}/>
+          <Note presenter="jl" notes={[]} />
           <Heading size={1} fit textColor="primary" caps>
             Configuration as Code
           </Heading>
           <Image src={images.toml} width={featureImageSize} />
         </Slide>
         <Slide bgColor="#fff">
-          <Note presenter="jl" notes={[]}/>
+          <Note
+            presenter="jl"
+            notes={[
+              "pas jam: razor, les CMS, le server side rendering",
+              "Tout est prébuild => rapide",
+              "Dans git",
+              "CMS Headless"
+            ]}
+          />
           <Image src={images.jamstackLogo} />
-          <Image src={images.jam} fit/>
+          <Image src={images.jam} fit />
         </Slide>
-        <Slide bgColor="#fff" transition={['zoom', 'slide']}>
-          <Note presenter="john" notes={[]}/>
+        <Slide bgColor="#fff" transition={["zoom", "slide"]}>
+          <Note presenter="john" notes={[]} />
           <Heading size={1} lineHeight={2} fill textColor="primary">
             Pricing
           </Heading>
@@ -277,22 +298,37 @@ export default class Presentation extends React.Component {
           </Table>
         </Slide>
         <Slide>
-          <Note presenter="john" notes={[]}/>
+          <Note presenter="john" notes={[]} />
           <Heading textColor="secondary" fit>
             Why use Netlify:
           </Heading>
           <Appear>
-            <Heading textAlign="left" size={3} lineHeight={3} textColor="secondary">
+            <Heading
+              textAlign="left"
+              size={3}
+              lineHeight={3}
+              textColor="secondary"
+            >
               &#10003; &nbsp; Automatic build
             </Heading>
           </Appear>
           <Appear>
-            <Heading textAlign="left" size={4} lineHeight={1} textColor="secondary">
+            <Heading
+              textAlign="left"
+              size={4}
+              lineHeight={1}
+              textColor="secondary"
+            >
               &#10003; &nbsp;Automatic deployment
             </Heading>
           </Appear>
           <Appear>
-            <Heading textAlign="left" size={3} lineHeight={3} textColor="secondary">
+            <Heading
+              textAlign="left"
+              size={3}
+              lineHeight={3}
+              textColor="secondary"
+            >
               &#10003; &nbsp;Previews
             </Heading>
           </Appear>
